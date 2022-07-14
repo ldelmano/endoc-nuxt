05a9a10c4988dfd2169904127b6879d78a207e57
Lucca Delmano <luccadelmano@gmail.com>
2022-07-14 01:51:52 -0300

    setup redirects

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,6 @@ import { defineNuxtConfig } from "nuxt";
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   target: "static",
-  generate: {
-    fallback: "index.html",
-  },
   shim: false,
   modules: ["@vueuse/nuxt", "@nuxt/content"],
   css: [
