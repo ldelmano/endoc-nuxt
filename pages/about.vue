bc3fe3227e3384a634384a7033fc9e35fb4c5b87
Lucca Delmano <luccadelmano@gmail.com>
2023-02-08 12:17:42 -0300

    update about us file structure

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,15 @@
+<script setup lang="ts">
+const { data } = await useAsyncData('aboutus', () => queryContent('about-us').find());
+
+useHead({
+  title: 'Endoc | About Us'
+})
+</script>
+
 <template>
   <div class="container">
     <h1>About</h1>
+
+
   </div>
 </template>
--- pages/become-a-seller.vue
+++ pages/become-a-seller.vue
@@ -1,3 +1,9 @@
+<script setup lang="ts">
+useHead({
+  title: 'Endoc | Become a Seller'
+})
+</script>
+
 <template>
   <div class="container">
     <h1>Become a seller</h1>
