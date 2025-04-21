70191c232dbb874f82a020f76457949489b9cf44
Lucca Delmano <luccadelmano@gmail.com>
2025-04-21 23:02:37 +0100

    update npm packages (no breaking changes)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
-import { glob } from "glob";
+import { sync } from "glob";
 import * as path from "path";
+import type { NuxtConfig } from "nuxt/schema";
 
 export const routeMap = {
   "/categories": "categories/*.md",
@@ -9,34 +10,38 @@ export const otherRoutes = [];
 
 export default defineNuxtConfig({
   app: { pageTransition: { name: "page", mode: "out-in" } },
-  ssr: false,
-  generate: {
-    routes: ([] as string[]).concat(getDynamicPaths(routeMap)),
+  nitro: {
+    prerender: {
+      routes: ([] as string[]).concat(getDynamicPaths(routeMap)),
+    },
   },
+
   modules: ["@vueuse/nuxt", "@nuxt/content"],
+
   css: [
     "~/assets/fonts/poppins/poppins.css",
     "~/assets/fonts/endoc-icons/endoc-icons.css",
     "~/assets/base.css",
     "~/node_modules/primeflex/primeflex.css",
   ],
+
   vue: {
     compilerOptions: {
-      isCustomElement: (tag) =>
+      isCustomElement: (tag: string) =>
         ["swiper-container", "swiper-slide"].includes(tag),
     },
   },
-});
 
-function getDynamicPaths(urlFilepathTable: any): string[] {
+  compatibilityDate: "2025-04-21",
+}) satisfies NuxtConfig;
+
+function getDynamicPaths(urlFilepathTable: Record<string, string>): string[] {
   return ([] as string[]).concat(
     ...Object.keys(urlFilepathTable).map((url) => {
       const filepathGlob = urlFilepathTable[url];
-      return glob
-        .sync(filepathGlob, { cwd: "content" })
-        .map((filepath: any) => {
-          return `${url}/${path.basename(filepath, ".md")}`;
-        });
+      return sync(filepathGlob, { cwd: "content" }).map((filepath: string) => {
+        return `${url}/${path.basename(filepath, ".md")}`;
+      });
     })
   );
 }
