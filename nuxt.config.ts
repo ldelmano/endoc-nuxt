18262b86a62a1877fed29f123caa6f1e0f5c2a8e
Lucca Delmano <luccadelmano@gmail.com>
2025-04-21 22:35:16 +0100

    adjust nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,17 +9,10 @@ export const otherRoutes = [];
 
 export default defineNuxtConfig({
   app: { pageTransition: { name: "page", mode: "out-in" } },
-  target: "static",
+  ssr: false,
   generate: {
     routes: ([] as string[]).concat(getDynamicPaths(routeMap)),
   },
-  iis: {
-    // merges in a pre-exisiting web.config file to the nitro default file
-    mergeConfig: true,
-    // overrides the default nitro web.config file all together
-    overrideConfig: false,
-  },
-  shim: false,
   modules: ["@vueuse/nuxt", "@nuxt/content"],
   css: [
     "~/assets/fonts/poppins/poppins.css",
